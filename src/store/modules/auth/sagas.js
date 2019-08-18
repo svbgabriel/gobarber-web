32539f5238b298664e764079a0c7a631b7b1ee6c
Gabriel Batista <svbgabriel@gmail.com>
2019-08-18 18:38:13 -0300

    Implementa toasts para exibir mensagens para o usuário

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
@@ -15,7 +16,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
 
@@ -23,6 +24,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verique seus dados');
     yield put(signFailure());
   }
 }
