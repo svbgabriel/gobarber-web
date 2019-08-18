094027e8a2a075640ac341d045ef8265de9c8e8f
Gabriel Batista <svbgabriel@gmail.com>
2019-08-18 15:21:32 -0300

    Configura o Root Import

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import Authlayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import Authlayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
