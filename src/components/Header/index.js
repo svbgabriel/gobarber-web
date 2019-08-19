ce5e713e26af45ff2849d6c1c41bee10175831c1
Gabriel Batista <svbgabriel@gmail.com>
2019-08-18 21:18:33 -0300

    Implementa a atualização dos dados do Header em tempo real

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -17,12 +20,15 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Gabriel Batista</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Gabriel Batista"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
